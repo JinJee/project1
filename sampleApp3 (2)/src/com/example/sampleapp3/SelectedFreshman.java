9ed860dcc269f37ec07dd51af4d0dec1b04b88f0
user <user@localhost.localdomain>
2015-03-04 21:33:57 +0900

    a

---
+++ sampleApp3 (2)/src/com/example/sampleapp3/SelectedFreshman.java
@@ -0,0 +1,12 @@
+package com.example.sampleapp3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SelectedFreshman extends Activity{
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_grade1);
+	}
+	
+}
