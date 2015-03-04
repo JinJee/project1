3031fe7a55ea88ef1fe93fe02b911b464310ce4c
user <user@localhost.localdomain>
2015-03-04 21:33:57 +0900

    a

---
+++ sampleApp3 (2)/sampleApp2/src/com/example/sampleapp2/SelectGrade.java
@@ -0,0 +1,16 @@
+package com.example.sampleapp2;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SelectGrade extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_selectgrade);
+	}
+	
+	
+}
